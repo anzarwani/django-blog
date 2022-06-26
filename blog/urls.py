2affcc70669d36426ed11d8340b0fd62b3de4f16
anzarhw <71497005+anzarhw@users.noreply.github.com>
2022-06-26 19:57:54 +0530

    urls

---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url 
+from django.urls import path
+from blog import views
+
+urlpatterns = [
+    path('/', views.PostListView.as_view(), name='post_list'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('post/<str:pk>', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new', views.CreatePostView.as_view(), name='post_new'),
+    path('post/<str:pk>/edit', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/<str:pk>/remove', views.PostDeleteView.as_view(), name='post_remove'),
+    path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+    path('post/<str:pk>/comment', views.add_comment_to_post.as_view(), name='add_comment_to_post'),
+    path('comment/<str:pk>/approve', views.comment_approve.as_view(), name='comment_approve'),
+    path('comment/<str:pk>/remove', views.comment_remove.as_view(), name='comment_remove'),
+    path('post/<str:pk>/publish', views.post_publish.as_view(), name='post_publish'),
+]
