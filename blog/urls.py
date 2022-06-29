f7755928679901fa359b6d10dc5a760b05fea6eb
anzarhw <71497005+anzarhw@users.noreply.github.com>
2022-06-29 09:42:59 +0530

    html

--- blog/urls.py
+++ blog/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('post/<str:pk>/edit', views.PostUpdateView.as_view(), name='post_edit'),
     path('post/<str:pk>/remove', views.PostDeleteView.as_view(), name='post_remove'),
     path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
-    path('post/<str:pk>/comment', views.add_comment_to_post.as_view(), name='add_comment_to_post'),
-    path('comment/<str:pk>/approve', views.comment_approve.as_view(), name='comment_approve'),
-    path('comment/<str:pk>/remove', views.comment_remove.as_view(), name='comment_remove'),
-    path('post/<str:pk>/publish', views.post_publish.as_view(), name='post_publish'),
+    path('post/<str:pk>/comment', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<str:pk>/approve', views.comment_approve, name='comment_approve'),
+    path('comment/<str:pk>/remove', views.comment_remove, name='comment_remove'),
+    path('post/<str:pk>/publish', views.post_publish, name='post_publish'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views
+from blog import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('accounts/login', views.login, name='login'),
-    path('accounts/logout', views.logout, name='logout', kwargs={'nextPage':'/'}),
+    path('accounts/login', LoginView.as_view(), name='login'),
+    path('accounts/logout', LogoutView.as_view(), name='logout', kwargs={'nextPage':'/'}),
 ]
